443fb6f784b71093bb7477f4d80b26544d807158
xavi Santamaria Viñas <xavisanta2@gmail.com>
2019-12-04 14:56:04 +0000

    Added text input for calories

--- mokoko/src/App.js
+++ mokoko/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Calories from './Calories'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiData: [],
+      calories: 0,
+      caloriesLeft: null,
+    }
+
+    this.handleInputCaloriesSubmit = this.handleInputCaloriesSubmit.bind(this);
+    this.caloriesInput = React.createRef();
+  }
+
+  handleInputCaloriesSubmit = event => {
+    event.preventDefault();
+    this.caloriesInputsetCalories(this.caloriesInput.current.value);
+  }
+
+  setCalories(cal) {
+    this.setState({ calories: cal })
+  }
+
+  render() {
+    return(  
+      <div className="App">
+        <Calories 
+          calories={this.state.calories} 
+          handleSubmit={this.handleInputCaloriesSubmit}
+          caloriesInput={this.caloriesInput}/>
+        
+        
+      </div>
+    );
+  }
 }
 
 export default App;
