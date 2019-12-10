8e6d14092384315c27c058cd38389a78031c1146
xavi Santamaria Viñas <xavisanta2@gmail.com>
2019-12-10 17:39:22 +0000

    Fix indent

--- mokoko/src/Pages/StatisticsPage.js
+++ mokoko/src/Pages/StatisticsPage.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
 export default class StatisticsPage extends Component {
-    render() {
-        return (
-            <h1>Statistics</h1>
-        )
-    }
+  render() {
+    return (
+      <h1>Statistics</h1>
+    )
+  }
 }
