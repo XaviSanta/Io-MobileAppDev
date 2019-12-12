6ef53d1a669b8bc0bbb3940054a94681b0716292
Uğur DURA <47089334+Twodor@users.noreply.github.com>
2019-12-12 10:11:18 +0000

    Update foodDB.js
    food database with
    -carbonhydrates(c)
    -proteins(p)
    -fruits and vegetables(v)
    -dairy(m)

--- mokoko/src/foodDB.js
+++ mokoko/src/foodDB.js
@@ -3,8 +3,9 @@ var l="lunch";
 var d="dinner";
 var c="carbonhydrate";
 var p="protein";
-var v="vitamin";
+var v="vitamin"; //vegetables also
 var f="fruit";
+var m="Dairy"
 export const foodDB =[
   {
     // The name of the food
@@ -145,6 +146,335 @@ export const foodDB =[
     calories:400 ,
     labels: [d,p],
   },
+  {
+    name: "2 Fish cake ",
+    calories:180 ,
+    labels: [d,p],
+  },
+  {
+    name: "5 Fish fingers",
+    calories: 250,
+    labels: [l,d,p],
+  },
+  {
+    name: "Gammon ",
+    calories:320  ,
+    labels: [d,p],
+  },
+  {
+    name: " Haddock Fresh",
+    calories:200  ,
+    labels: [d,p],
+  },
+  {
+    name: " 10 slice Ham",
+    calories:60,
+    labels: [,p],
+  },
+  {
+    name: " 100 gr Liver ",
+    calories: 150 ,
+    labels: [d,p],
+  },
+  {
+    name: "100 gr Lamp",
+    calories:  300,
+    labels: [d,p],
+  },
+  {
+    name: "Lobster boiled ",
+    calories: 200 ,
+    labels: [d,p],
+  },
+  {
+    name: "100 gr Pork  ",
+    calories: 290 ,
+    labels: [l,d,p],
+  },
+  {
+    name: "Pork Pie",
+    calories: 450 ,
+    labels: [d,p],
+  },
+  {
+    name: " 100 gr Rabbit meat ",
+    calories: 180 ,
+    labels: [d,p],
+  },
+  {
+    name: "200 gr fresh Salmon ",
+    calories: 360 ,
+    labels: [d,p],
+  },
+  {
+    name: "100 gr Pork fried sausage ",
+    calories: 320 ,
+    labels: [l,d,p],
+  },
+  {
+    name: " 100 gr sausage roll",
+    calories:480 ,
+    labels: [,p],
+  },
+  {
+    name: "120 gr Steak  & Kidney Pie ",
+    calories:400,
+    labels: [d,p],
+  },
+  {
+    name: " 200 gr Fresh Trout ",
+    calories: 200 ,
+    labels: [d,p],
+  },
+  {
+    name: "100 gr Oil tinned Tuna ",
+    calories:  180,
+    labels: [l,d,p],
+  },
+  {
+    name: "200 gr Turkey ",
+    calories: 320,
+    labels: [l,d,p],
+  },
+  {
+    name: "1 boiled egg",
+    calories: 90,
+    labels: [b,p],
+  },
+  {
+    name: "1 fried egg",
+    calories: 120,
+    labels: [b,p],
+  },
+  {
+    name: "100 gr Veal ",
+    calories:240,
+    labels: [l,d,p],
+  },
+  {
+    name: "1 Banana",
+    calories: 107,
+    labels: [b,l,d,v],
+  },
+  {
+    name: "200 gr Baked beans  ",
+    calories: 170 ,
+    labels: [l,d,v],
+  },
+  {
+    name: "100 gr Blackberries ",
+    calories:25,
+    labels: [b,l,d,v],
+  },
+  {
+    name: "100 gr blackcurrant ",
+    calories:30 ,
+    labels: [b,l,v],
+  },
+  {
+    name: "100 gr Broccoli ",
+    calories: 32 ,
+    labels: [l,d,v],
+  },
+  {
+    name: "200 gr Cabbage ",
+    calories: 40 ,
+    labels: [l,d,v],
+  },
+  {
+    name: " 400 gr carrot",
+    calories: 100 ,
+    labels: [l.d,v],
+  },
+  {
+    name: "100 gr Cherry ",
+    calories:50,
+    labels: [b,l,d,v],
+  },
+  {
+    name: "1 bowl Courgette soup",
+    calories: 50,
+    labels: [l,d,v],
+  },
+  {
+    name: " 2 Cucumber with salt ",
+    calories: 6,
+    labels: [b.l.d,v],
+  },
+  {
+    name: "5 pieces dates",
+    calories:235,
+    labels: [l,d,b,v],
+  },
+  {
+    name: "100 gr Grape",
+    calories:62,
+    labels: [b,l,d,v],
+  },
+  {
+    name: "1 Grapefruit",
+    calories: 32,
+    labels: [b,l,d,v],
+  },
+  {
+    name: " 3 Kiwi",
+    calories: 150 ,
+    labels: [b,l,d,v],
+  },
+  {
+    name: "3 pieces Lettuce",
+    calories:15,
+    labels: [l,v],
+  },
+  {
+    name: "1 slice melon",
+    calories: 28,
+    labels: [b,l,d,v],
+  },
+  {
+    name: "5 Mushrooms boiled",
+    calories: 60,
+    labels: [l,d,v],
+  },
+  {
+    name: "5 Mushrooms fried ",
+    calories: 600,
+    labels: [l,d,v],
+  },
+  {
+    name: "50 g olives",
+    calories:  40,
+    labels: [b,v],
+  },
+  {
+    name: " 1 Orange",
+    calories: 40 ,
+    labels: [b,l,d,v],
+  },
+  {
+    name: " 1 Peach",
+    calories: 35,
+    labels: [b,l,d,v],
+  },
+  {
+    name: " 1 Pear",
+    calories: 45  ,
+    labels: [b,l,d,v],
+  },
+  {
+    name: "100 gr Pineapple",
+    calories:40,
+    labels: [b,l,d,v],
+  },
+  {
+    name: "200 gr  Spinach",
+    calories:16 ,
+    labels: [l,d,v],
+  },
+  {
+    name: "5 pieces strawberries",
+    calories:50,
+    labels: [b,l,d,v],
+  },
+  {
+    name: "100 gr  Sweetcorn",
+    calories:130,
+    labels: [l,d,v],
+  },
+  {
+    name: "200 gr Tomato",
+    calories:40,
+    labels: [b,l,d,v],
+  },
+  {
+    name: "1 Avacado",
+    calories:150,
+    labels: [b,l,d,v],
+  },
+  {
+    name: "1 mango",
+    calories:40,
+    labels: [b,l,d,v],
+  },
+  {
+    name: "50 gr sultanas",
+    calories:70,
+    labels: [b,l,d,v],
+  },
+ 
+  {
+    name: "6 pieces Cherry tomato",
+    calories:36,
+    labels: [b,l,d,v],
+  },
+  {
+    name: "25 gr cheese",
+    calories:110,
+    labels: [ b,m],
+  },
+  {
+    name: " 50 gr Cheaddar ",
+    calories:130,
+    labels: [ b,l,m],
+  },
+  {
+    name: "50 gr cottage cheese ",
+    calories:49,
+    labels: [ b,m],
+  },
+  {
+    name: "50 gr cream cheese ",
+    calories:200,
+    labels: [ b,m],
+  },
+  {
+    name: "100 gr Fromage frais",
+    calories:125,
+    labels: [b ,m],
+  },
+  {
+    name: "125 gr ice cream",
+    calories:200,
+    labels: [ l,d,m],
+  },
+  {
+    name: "250 ml Whole milk ",
+    calories:175,
+    labels: [b ,m],
+  },
+  {
+    name: "250 ml Semi skimmed Milk",
+    calories:125,
+    labels: [b ,m],
+  },
+  {
+    name: "250 ml Skimmed milk",
+    calories:95,
+    labels: [b ,m],
+  },
+  {
+    name: "250 ml Soya milk",
+    calories:90,
+    labels: [ b,m],
+  },
+  {
+    name: " Omelette with cheese(1 egg 50 gr cheese)",
+    calories:300,
+    labels: [b ,m],
+  },
+  {
+    name: "200 gr Natural Yogurt",
+    calories:120,
+    labels: [b,l,d ,m],
+  },
+ 
+
+
+
+
+
+ 
+
 
 
 
